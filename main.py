ca626d2b0aa4bac3d7e85abb5d721821d7493bc5
KewKartik <108450560+kewkartik@users.noreply.github.com>
2024-02-19 19:53:01 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+from PIL import Image, ImageFont, ImageDraw
+
+# Global Variables
+FONT_FILE = ImageFont.truetype(r'./GreatVibes-Regular.ttf', 80)
+
+FONT_COLOR = "#28282B"
+
+template = Image.open(r'template.png')
+WIDTH, HEIGHT = template.size
+
+def make_certificates(name):
+    '''Function to save certificates as a .png file'''
+
+    image_source = Image.open(r'template.png')
+    draw = ImageDraw.Draw(image_source)
+
+    # Calculate the width and height of the text dynamically
+    name_width, name_height = 450, 15
+
+    # Placing it in the center, then making some adjustments.
+    # Adjust the y-coordinate by subtracting a fraction of the text's height
+    draw.text(((WIDTH - name_width) / 2, (HEIGHT - name_height) / 2 - name_height * 0.1), name, fill=FONT_COLOR, font=FONT_FILE)
+
+    # Define the directory path
+    dir_path = "./out/"
+
+    # Check if the directory exists, if not, create it
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    # Saving the certificates in a different directory.
+    image_source.save(dir_path + name + ".png")
+    print('Saving Certificate of:', name)
+
+
+if __name__ == "__main__":
+
+    names = []
+    with open('name.txt') as f:
+        content = f.readlines()
+        for item in content:
+            names.append(item[:-1].title())
+
+    for name in names:
+        make_certificates(name)
+
+    print(len(names), "certificates done.")
